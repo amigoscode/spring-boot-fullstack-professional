d673c0b4bab21e1196dbb183979a8dff9cc9b9a6
amigoscode <amigoscode@gmail.com>
2021-03-13 02:23:35 +0000

    added noreferrer

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -228,6 +228,7 @@ function App() {
                 />
                 <Divider>
                     <a
+                        rel="noopener noreferrer"
                         target="_blank"
                         href="https://amigoscode.com/p/full-stack-spring-boot-react">
                         Click here to access Fullstack Spring Boot & React for professionals
