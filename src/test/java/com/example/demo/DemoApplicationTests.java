15b66d706d021fc394ba842a752cf029d1518a23
amigoscode <amigoscode@gmail.com>
2021-03-13 00:25:10 +0000

    make test fail

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Ooops test failed");
 	}
 
 }
