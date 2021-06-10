501cf2bcaed11b1d5c4761cdc12dbb7336c18ffc
ahamilto99 <ajhamilton15@hotmail.com>
2021-06-10 06:30:57 -0400

    Completed Module 2

--- src/frontend/src/client.js
+++ src/frontend/src/client.js
@@ -1,15 +1,13 @@
 import fetch from 'unfetch';
 
 const checkStatus = response => {
-    if (response.ok) {
-        return response;
-    }
-    // convert non-2xx HTTP responses into errors:
-    const error = new Error(response.statusText);
-    error.response = response;
-    return Promise.reject(error);
+	if (response.ok) {
+		return response;
+	}
+	// converts non-2xx HTTP responses into errors
+	const error = new Error(response.statusText);
+	error.response = response;
+	return Promise.reject(error);
 }
 
-export const getAllStudents = () =>
-    fetch("api/v1/students")
-        .then(checkStatus);
+export const getAllStudents = () => fetch('api/v1/students').then(checkStatus);
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App';  // <App /> is the mounting point for our application
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
