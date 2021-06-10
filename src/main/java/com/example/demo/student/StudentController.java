8dd5dbbb417f8953c842c6243fa728a1c370a264
ahamilto99 <ajhamilton15@hotmail.com>
2021-06-10 16:07:13 -0400

    Completed Module 3

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,11 +1,11 @@
 package com.example.demo.student;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
